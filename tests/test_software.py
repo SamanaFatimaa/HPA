d3a9a54df9b33e94d37c82439a88fdc6751f44a2
Samana Fatima <S22sfati@uni-bonn.de>
2024-10-22 11:55:09 +0200

    Update test_software.py

--- tests/test_software.py
+++ tests/test_software.py
@@ -1,21 +1,23 @@
+from hpa.tools import Analyzer  # type: ignore
 
-from hpa.tools import Analyzer
+test_file_path = "./tests/data/normal_tissue_test.tsv"
 
-test_file_path = "./data/normal_tissue_test.tsv"
 
 def test_number_of_genes():
     """Test if get_number_of_genes return the correct number of distinct genes."""
-    analyzer = Analyzer(path=test_file_path)
+    analyzer = Analyzer(url_or_path=test_file_path)
     assert analyzer.get_number_of_genes() == 3
+
 
 def test_distinct_genes():
     """Test if get_distinct_genes returns all distinct genes."""
-    analyzer = Analyzer(path=test_file_path)
+    analyzer = Analyzer(url_or_path=test_file_path)
     expected_data = ["ENSG00000000003", "ENSG00000000005", "ENSG00000000419"]
     assert expected_data == analyzer.get_distinct_genes()
 
+
 def test_tissues_by_gene_name():
     """Test if get_tissues_by_gene_name returns distinct tissues as list of strings."""
-    analyzer = Analyzer(path=test_file_path)
+    analyzer = Analyzer(url_or_path=test_file_path)
     expected_data: list[str] = ["adipose tissue", "vagina"]
     assert expected_data == analyzer.get_tissues_by_gene_name("TSPAN6")
